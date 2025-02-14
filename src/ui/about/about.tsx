f574594dd8c7b7bcb13ed4066c3a7f334940cadb
grrowe <groweiginger10@gmail.com>
2025-02-13 20:12:19 -0500

    initial draft for about page

--- src/ui/about/about.tsx
+++ src/ui/about/about.tsx
@@ -1,11 +1,21 @@
 import "./about.css";
 
 function About() {
-  return (
-    <>
-    Blah blah my name is graham
-    </>
-  );
+    return (
+        <>
+            <p>
+                Greetings, fellow adventurer! I am a software engineer based in the circle city of Indianapolis, embarking on a journey through the world of code and technology. 
+                
+                My quest began with a spark of curiosity, leveling up my skills in various programming languages, frameworks, and tools to solve complex challenges and build powerful software solutions.
+
+                Along the way, I've unlocked key abilities in languages like JavaScript/Typescript, Python, and Ruby, equipped with the power of modern frameworks and tools to forge sleek and efficient systems. Whether it's designing user-friendly interfaces, optimizing back-end performance, or ensuring security across the board, every line of code is a step toward mastering the craft.
+
+                But the journey never truly ends. Each day is an opportunity to gain new knowledge, conquer fresh challenges, and push the boundaries of my abilities. The world of software engineering is vast, and I continue to learn, adapt, and grow, like a hero on an endless quest to sharpen my skills and help others achieve their goals.
+
+                If you need a partner on your own coding adventure, let's team up and take on the next challenge together!
+            </p>
+        </>
+    );
 }
 
 export default About;
