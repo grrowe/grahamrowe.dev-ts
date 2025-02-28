a7092fd20fea8e8a6a074b5fadbb95a498441a77
Graham Rowe <groweiginger10@gmail.com>
2025-02-28 13:18:01 -0500

    freshen up resume page

--- src/ui/resume/resume.tsx
+++ src/ui/resume/resume.tsx
@@ -22,27 +22,33 @@ const Resume = () => {
               </h3>
               <li className="tab">
                 <p>
-                  Serve as a technical support to ensure customer success and
-                  satisfaction by identifying and resolving issues related to
-                  product performance and reliability
+                  Investigated and resolved over 30 customer-reported incidents
+                  and tickets per week, utilizing logs, monitoring tools, and
+                  debugging techniques to ensure system reliability
                 </p>
               </li>
               <li className="tab">
                 <p>
-                  Designed, implemented, and optimized full-stack applications (
-                  <a href="https://vite.dev/" target="_blank">
-                    Vite
-                  </a>
-                  ,{" "}
-                  <a href="https://emberjs.com/" target="_blank">
-                    Ember.js
-                  </a>
-                  , and{" "}
-                  <a href="https://www.ruby-lang.org/en/" target="_blank">
-                    Ruby
-                  </a>
-                  ), ensuring robust functionality and seamless user
-                  experiences.
+                  Developed custom scripts/tools as needed to automate routine
+                  tasks, increasing overall team productivity and efficiency.
+                </p>
+              </li>
+              <li className="tab">
+                <p>
+                  Collaborated with cross-functional teams to develop, test, and
+                  deploy scalable software solutions.
+                </p>
+              </li>
+              <li className="tab">
+                <p>
+                  Advocate for customers by providing feedback to product and
+                  engineering teams to enhance the user experience.
+                </p>
+              </li>
+              <li className="tab">
+                <p>
+                  Analyze trends in customer issues to proactively drive
+                  improvements in system design and reliability.
                 </p>
               </li>
               <div className="darkColor negTab">
@@ -52,59 +58,68 @@ const Resume = () => {
 
             <div>
               <h3 className="negTab">
-                IVM, Inc. Indianapolis, IN. - Frontend Software Engineer
+                IVM, Inc. Indianapolis, IN. - Front End Software Engineer
               </h3>
               <li className="tab">
                 <p>
-                  Implement and maintain interactive, customer-facing apps with{" "}
+                  Develop, test, and maintain scalable front-end applications
+                  using{" "}
                   <a href="https://vuejs.org/" target="_blank">
-                    Vue.js
+                    Vue.js.
                   </a>
-                  , and{" "}
-                  <a href="https://vuex.vuejs.org/" target="_blank">
-                    Vuex
-                  </a>{" "}
-                  focusing on reliability and user experience.
+                </p>
+              </li>
+              <li className="tab">
+                <p>
+                  Provided ongoing support for maintenance tasks such as bug
+                  fixes or feature updates to deliver a consistently
+                  high-quality product.
+                </p>
+              </li>
+              <li className="tab">
+                <p>
+                  Developed and maintained productivity tools that streamlined
+                  internal processes, reducing manual workload by 40%
+                </p>
+              </li>
+              <li className="tab">
+                <p>
+                  Write reusable and maintainable Vue.js components while
+                  following best practices in component-based architecture.
                 </p>
               </li>
               <div className="darkColor negTab">March 2022 - Auguest 2022</div>
             </div>
 
             <div>
-              <h3 className="negTab">Irving Materials Inc. Greenfield, IN.</h3>
+              <h3 className="negTab">Irving Materials Inc. Greenfield, IN. - Full Stack Software Engineer</h3>
 
               <li className="tab">
                 <p>
-                  Engineered and deployed scalable internal applications
-                  utilized across the organization, enhancing cross-departmental
-                  functionality and streamlining workflows by leveraging{" "}
-                  <a href="https://react.dev/" target="_blank">
-                    React
-                  </a>
-                  ,{" "}
-                  <a href="https://redux.js.org/" target="_blank">
-                    Redux
-                  </a>
-                  ,{" "}
-                  <a href="https://nodejs.org/en" target="_blank">
-                    Node JS
-                  </a>
-                  , and{" "}
-                  <a
-                    href="https://aws.amazon.com/"
-                    target="_
-                  // blank"
-                  >
-                    AWS
-                  </a>
-                  .
+                  Created user-friendly applications to support employees in
+                  improving efficiency and task management.
+                </p>
+              </li>
+              <li className="tab">
+                <p>
+                  Developed secure, scalable software solutions by utilizing
+                  industry best practices for coding and architecture.
+                </p>
+              </li>
+              <li className="tab">
+                <p>
+                  Integrated third-party APIs to enhance overall application
+                  functionality and improve user experience.
+                </p>
+              </li>
+              <li className="tab">
+                <p>
+                  Collaborated with cross-functional teams to deliver
+                  high-quality products on time and within budget constraints.
                 </p>
               </li>
 
-              <h4 className="noMarginTop">Solutions Developer</h4>
-              <div className="darkColor">May 2021 - March 2022</div>
-              <h4 className="noMarginTop">Solutions Development Intern</h4>
-              <div className="darkColor">March 2020 - May 2021</div>
+              <div className="darkColor">March 2020 - March 2022</div>
             </div>
           </ul>
         </div>
@@ -142,15 +157,16 @@ const Resume = () => {
         <div className="skills">
           <h3>Skills</h3>
           <ul className="skillsList">
-            <li>
-              Javascript (ES6, ReactJS + Redux, jQuery, Vue.JS, Angular, Node
-              JS, Express.js, Ember.js)
-            </li>
-            <li>Databases (SQL, PostgresSQL, NoSQL DBs, MongoDB, Oracle)</li>
-            <li>AWS and Azure for cloud computing platforms</li>
-            <li>Web Design (SCSS, Animation, D3.js, Figma, Adobe XD)</li>
-            <li>Version control (git)</li>
-            <li>Package Management (yarn, npm)</li>
+            <li>Incident management and Log analysis</li>
+            <li>API design and integration</li>
+            <li>React.js and Vue.js</li>
+            <li>Node.js</li>
+            <li>Ruby on Rails</li>
+            <li>Typescript</li>
+            <li>Software Development Lifecycle</li>
+            <li>SQL and PostreSQL</li>
+            <li>Strong debugging skills</li>
+            <li>RESTful API</li>
           </ul>
 
           <DownloadResume />
